bcb5be728d7850d37c783dc43b839036cf7d8e40
lhalcomb <lhalcomb@drury.edu>
2023-02-15 18:44:57 -0600

    Layden's Changes- Added LineSize

--- DELTA_CUBE.py
+++ DELTA_CUBE.py
@@ -1,5 +1,4 @@
 #Author: Taylor Sloan, Layden Halcomb 
-
 import random, math, pygame
 winwidth = 800  # width of window
 winheight = 600  # height of window
@@ -7,6 +6,7 @@ background = (255,255,255)
 widthX = 200
 positionX = winwidth / 2
 positionY = winheight / 1.2
+linewidth = 5
 
 px1 = positionX - widthX
 px2 = positionX-93*widthX/300
@@ -23,7 +23,7 @@ py6 = positionY-widthX*2
 screen = pygame.display.set_mode((winwidth, winheight))
 clock = pygame.time.Clock()
 pygame.init()
-pygame.display.set_caption("Triangles v1")
+pygame.display.set_caption("DELTA CUBE")
 pygame.event.set_allowed([pygame.QUIT, pygame.KEYUP, pygame.KEYDOWN])
 
 quit = False
@@ -42,55 +42,55 @@ while not quit:
     screen.fill(background)
     #1 Bottom Right
     pygame.draw.line(
-        screen, (0, 0, 0), (px3, py1), (px5, py2)
+        screen, (0, 0, 0), (px3, py1), (px5, py2), (linewidth)
     )
     #2 Bottom Left
     pygame.draw.line(
-        screen, (0, 0, 0), (px3, py1), (px1, py2)
+        screen, (0, 0, 0), (px3, py1), (px1, py2), (linewidth)
     )
     #3 Right
     pygame.draw.line(
-        screen, (0, 0, 0), (px5, py2), (px5, py5)
+        screen, (0, 0, 0), (px5, py2), (px5, py5), (linewidth)
     )
     #4 Left
     pygame.draw.line(
-        screen, (0, 0, 0), (px1, py2), (px1, py5)
+        screen, (0, 0, 0), (px1, py2), (px1, py5), (linewidth)
     )
     #5 Top Right
     pygame.draw.line(
-        screen, (0, 0, 0), (px5, py5), (px3, py6)
+        screen, (0, 0, 0), (px5, py5), (px3, py6), (linewidth)
     )
     #6 Top Left
     pygame.draw.line(
-        screen, (0, 0, 0), (px1, py5), (px3, py6)
+        screen, (0, 0, 0), (px1, py5), (px3, py6), (linewidth)
     )
     #7 Low Mid Right
     pygame.draw.line(
-        screen, (0, 0, 0), (px3, py3), (px5, py2)
+        screen, (0, 0, 0), (px3, py3), (px5, py2), (linewidth)
     )
     #8 Low Mid Left
     pygame.draw.line(
-        screen, (0, 0, 0), (px3, py3), (px1, py2)
+        screen, (0, 0, 0), (px3, py3), (px1, py2), (linewidth)
     )
     #9 Mid
     pygame.draw.line(
-        screen, (0, 0, 0), (px3, py3), (px3, py6)
+        screen, (0, 0, 0), (px3, py3), (px3, py6), (linewidth)
     )
     #10 Right Diagonal
     pygame.draw.line(
-        screen, (0, 0, 0), (px5, py2), (px3, py6)
+        screen, (0, 0, 0), (px5, py2), (px3, py6), (linewidth)
     )
     #11 Left Diagonal
     pygame.draw.line(
-        screen, (0, 0, 0), (px1, py2), (px3, py6)
+        screen, (0, 0, 0), (px1, py2), (px3, py6), (linewidth)
     )
     #12 High Mid Left
     pygame.draw.line(
-        screen, (0, 0, 0), (px1, py5), (px2, py4)
+        screen, (0, 0, 0), (px1, py5), (px2, py4), (linewidth)
     )
     #13 High Mid Right
     pygame.draw.line(
-        screen, (0, 0, 0), (px5, py5), (px4, py4)
+        screen, (0, 0, 0), (px5, py5), (px4, py4), (linewidth)
     )
 
     clock.tick(60)
