ae783667680b4c1c5bc8793d215a664c89aa896d
lhalcomb <lhalcomb@drury.edu>
2023-02-15 21:32:35 -0600

    Update Generative_Art_Piece.py

--- Generative_Art_Piece.py
+++ Generative_Art_Piece.py
@@ -1,5 +1,5 @@
-import random, math, pygame
-
+#Generative_Art_Piece.py 
+import pygame, math, random 
 def radians(degrees):
     """convert degrees to radians"""
     return math.pi / 180 * degrees
@@ -53,12 +53,25 @@ class Node:
 winwidth = 800  # width of window
 winheight = 600  # height of window
 background = (5, 5, 5)  # this is close to black
+winwidth = 800  # width of window
+winheight = 600  # height of window
+background = (5, 5, 5)  # this is close to black
 
 # set generative parameters
 num_nodes = 400
 node_radius = 0
 thresh = 1800
+# set generative parameters
+num_nodes = 400
+node_radius = 0
+thresh = 1800
 
+# initialize pygame
+screen = pygame.display.set_mode((winwidth, winheight))
+clock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption("Triangles v1")
+pygame.event.set_allowed([pygame.QUIT, pygame.KEYUP, pygame.KEYDOWN])
 # initialize pygame
 screen = pygame.display.set_mode((winwidth, winheight))
 clock = pygame.time.Clock()
@@ -74,7 +87,19 @@ for i in range(num_nodes):
     speed = random.randint(150, 200) / 600
     angle = radians(random.randint(0, 359))
     nodes.append(Node(x, y, speed, angle))
+# create nodes
+nodes = []
+for i in range(num_nodes):
+    x = random.randint(node_radius, winwidth - node_radius)
+    y = random.randint(node_radius, winheight - node_radius)
+    speed = random.randint(150, 200) / 600
+    angle = radians(random.randint(0, 359))
+    nodes.append(Node(x, y, speed, angle))
 
+# the game loop: (press q to quit)
+quit = False
+reset = 0
+while not quit:
 # the game loop: (press q to quit)
 quit = False
 reset = 0
@@ -90,7 +115,22 @@ while not quit:
                 break
     if quit:
         break
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit = True
+            break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                quit = True
+                break
+    if quit:
+        break
 
+    screen.fill(background)
+    for node in nodes:
+        node.move()
+        node.reflect()
+        node.draw()
     screen.fill(background)
     for node in nodes:
         node.move()
@@ -126,9 +166,42 @@ while not quit:
                     screen, blue((thresh - d_squared) / thresh), (x1, y1), (x2, y2)
                 )
     reset += 1
+    if reset % 30 == 0:
+        nodes = sorted(nodes, key = lambda node: node.x)
+        listPerNode = []
+        for i, node1 in enumerate(nodes):
+            dictPerNode = {}
+            run = True
+            for node2 in nodes[i:]:
+                if (node1.x > node2.x - 150):
+                    x1, y1 = node1.x, node1.y
+                    x2, y2 = node2.x, node2.y
+                    d_squared = (x1 - x2) ** 2 + (y1 - y2) ** 2
+                    if d_squared < thresh * 2:
+                        dictPerNode[node2] = node1
+                    elif node1.x > node2.x + 150:
+                        break
+            listPerNode.append(dictPerNode)
+        reset += 1
+    for m in range(0, len(listPerNode)):
+        dictPerNode = listPerNode[m]
+        for node1 in dictPerNode:
+            node2 = dictPerNode[node1]
+            x1, y1 = node1.x, node1.y
+            x2, y2 = node2.x, node2.y
+            d_squared = (x1 - x2) ** 2 + (y1 - y2) ** 2
+            if d_squared < thresh:
+                pygame.draw.aaline(
+                    screen, blue((thresh - d_squared) / thresh), (x1, y1), (x2, y2)
+                )
+    reset += 1
 
     clock.tick(120)
     pygame.display.flip()
     print(clock.get_fps())
+    clock.tick(120)
+    pygame.display.flip()
+    print(clock.get_fps())
 
+pygame.quit()
 pygame.quit()
